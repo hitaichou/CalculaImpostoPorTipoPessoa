f32f8151f4b5111a91d298a3d9070374ed14fad0
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-07-16 19:52:12 -0300

    Adicionar arquivos de projeto.

---
+++ CalculaImpostoPorTipoPessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculaImpostoPorTipoPessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
